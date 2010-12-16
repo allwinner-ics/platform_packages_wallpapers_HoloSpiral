d75b28facf7ebef977f6b240bf1519ac6cbca691
Adam Lesinski <adamlesinski@google.com>
2010-12-16 09:59:16 -0800

    Changed package name
    Change-Id: I1e585b3c9525fe331ce04fee57ac1846642f3597

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
 
-LOCAL_PACKAGE_NAME := HoloSpiral
+LOCAL_PACKAGE_NAME := HoloSpiralWallpaper
 LOCAL_CERTIFICATE := shared
 
 include $(BUILD_PACKAGE)
